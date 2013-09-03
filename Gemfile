077c09e8e98ee7661c4503969da7a22aae74c3cb
Juan Pemberthy <jpemberthy@gmail.com>
2013-09-02 21:19:56 -0700

    preparation for heroku

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
+ruby '1.9.3'
 
 gem 'rails', '4.0.0'
 gem 'mysql2'
@@ -24,6 +25,8 @@ end
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 gem 'debugger', group: [:development, :test]
+
+gem 'rails_12factor', group: :production
 
 #core
 gem 'pismo'
--- config/initializers/imgkit.rb
+++ config/initializers/imgkit.rb
@@ -1,6 +1,11 @@
 # config/initializers/imgkit.rb
 IMGKit.configure do |config|
-  config.wkhtmltoimage = '/usr/local/bin/wkhtmltoimage'
+  if Rails.env.production?
+    config.wkhtmltoimage = Rails.root.join('bin', 'wkhtmltoimage').to_s
+  else
+    config.wkhtmltoimage = '/usr/local/bin/wkhtmltoimage'
+  end
+
   config.default_format = :png
   config.default_options = {
     :quality => 100,
